a2db4cd2697b64e047599c6ccb4fcb3714276d63
Kai Gustafson <kjgustafson1996@pointloma.edu>
2018-05-09 15:01:24 -0700

    first android build, track pieces made, player controller made

---
+++ CurbBalance/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    public Rigidbody rb;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+	}
+	
+	void Update ()
+    {
+        speed = speed + .01f;
+
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right *Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        transform.Translate(new Vector3(Input.acceleration.x, 0, 0) * Time.deltaTime * speed);
+	}
+}
